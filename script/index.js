6501457ce061d9a44a74f3eb09cbc9e58124f73d
LswaN58 <lwswanson2@wisc.edu>
2022-01-17 11:02:03 -0600

    Remove some unnecessary whitespace.

--- script/index.js
+++ script/index.js
@@ -17,17 +17,6 @@ function pageload() {
     document.getElementById("txt_mathjax").innerHTML = escapeHTML(str);
     str = "$$\\int_a^b x^2 = \\frac{1}{3}x^3 |_a^b$$";
     document.getElementById("txt_mjax_ex").innerHTML = escapeHTML(str);
-    // Bootstrap include
-    str = `<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">`;
-    document.getElementById("txt_bootstrap_css").innerHTML = escapeHTML(str);
-    str = `<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" crossorigin="anonymous"></script>`;
-    document.getElementById("txt_bootstrap_js").innerHTML = escapeHTML(str);
-    str = `<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" crossorigin="anonymous"></script>`;
-    document.getElementById("txt_bootstrap_popper").innerHTML = escapeHTML(str);
-    // Bootstrap code
-    str = `<p class="text-primary">Text made blue by Bootstrap</p>`;
-    document.getElementById("txt_bootstrap_ex").innerHTML = escapeHTML(str);
-
 }
 
 // jQuery example code
